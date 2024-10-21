cfb42c0105af440583efbce9b0b711d0b3a432e9
nitipath <aumnitipath@outlook.com>
2024-10-21 18:54:15 +0700

    add product Controller,Service,Module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log('this =>', this); //this => AppController { appService: AppService {} }
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //NOTE check ถ้ามี process.env.PORT ก็จะใช้ค่านี้แต่ถ้าไม่มีจะ default 3000
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
